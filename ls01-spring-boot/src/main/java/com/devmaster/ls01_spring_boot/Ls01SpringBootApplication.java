eef18bf9962f314f6e21bfa94762ff374518405e
unknown <nguyengiaduy2005@gmail.com>
2025-10-20 21:08:25 +0700

    update

--- ls01-spring-boot/src/main/java/com/devmaster/ls01_spring_boot/Ls01SpringBootApplication.java
+++ ls01-spring-boot/src/main/java/com/devmaster/ls01_spring_boot/Ls01SpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ls01SpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ls01SpringBootApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Ls01SpringBootApplication.class, args);
+        System.out.println("Hello em");
+    }
 }
